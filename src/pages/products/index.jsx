260be4b42cdb7b7edc6ab447f5f1279d20b0499c
Maksim Dorogoi <monarch2013@mail.ru>
2024-10-29 16:16:08 +1100

    мэппинг массива productsList

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,6 +1,7 @@
 import "./products.css"
 import Card from '../../components/elements/Card'
 import Basket from "../../components/UI/Basket";
+import {productsList} from "../../products.js"
 
 
 function Products() {
@@ -20,71 +21,19 @@ function Products() {
 
                 <div className="productsList">
                
-                    <Card
-                        url="/images/1.png"
-                        title="Устрицы по рокфеллеровски"
-                        description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры "
-                        price="2 700"
-                        weight="500"
-                        
-                    />
-                     
+                {productsList.map(item=>{
+                        return(
+                        <Card
+                        key={item.id}
+                        url={item.url}
+                        title={item.title}
+                        description={item.description}
+                        price={item.price}
+                        weight={item.weight}
+                        />
+                        )
+                })}
 
-                    <Card
-                        url="/images/2.png"
-                        title="Свиные ребрышки на гриле с зеленью"
-                        description="Не следует, однако забывать, что реализация намеченных плановых"
-                        price="1 600"
-                        weight="750"
-                    />
-
-                    <Card
-                        url="/images/3.png"
-                        title="Креветки по-королевски в лимонном соке"
-                        description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры обеспечивает широкому кругу"
-                        price="1 820"
-                        weight="7"
-                    />
-
-                    <Card
-                    url="/images/1.png"
-                        title="Устрицы по рокфеллеровски"
-                        description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры "
-                        price="2 700"
-                        weight="500"
-                    />
-
-                    <Card
-                    url="/images/1.png"
-                        title="Устрицы по рокфеллеровски"
-                        description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры "
-                        price="2 700"
-                        weight="500"
-                    />
-
-                    <Card
-                    url="/images/2.png"
-                        title="Свиные ребрышки на гриле с зеленью"
-                        description="Не следует, однако забывать, что реализация намеченных плановых"
-                        price="1 600"
-                        weight="750"
-                    />
-
-                    <Card
-                    url="/images/3.png"
-                        title="Креветки по-королевски в лимонном соке"
-                        description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры обеспечивает широкому кругу"
-                        price="1 820"
-                        weight="7"
-                    />
-
-                    <Card
-                    url="/images/1.png"
-                        title="Устрицы по рокфеллеровски"
-                        description="Значимость этих проблем настолько очевидна, что укрепление и развитие структуры "
-                        price="2 700"
-                        weight="500"
-                    />
 
                 </div>
 
